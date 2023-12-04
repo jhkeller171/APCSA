249da351104e2b6587a8619a2564b70c280ec84a
James Keller <119902101+jhkeller171@users.noreply.github.com>
2023-12-04 10:19:02 -0500

    Create bobThere.java

---
+++ Coding Bat/String-2/bobThere.java
@@ -0,0 +1,20 @@
+/*
+Return true if the given string contains a "bob" string, but where the middle 'o' char can be any char.
+
+
+bobThere("abcbob") → true
+bobThere("b9b") → true
+bobThere("bac") → false
+*/
+
+public boolean bobThere(String str) {
+  if (str.length() >= 3){
+    
+  for (int i=0;i<str.length()-2;i++){
+    if (str.substring(i,i+1).equals("b") && str.substring(i+2,i+3).equals("b")){
+      return true;
+    }
+  }
+  }
+  return false;
+}
