a305057cf832bb10a8393b37ddf7ae29333700bf
James Keller <119902101+jhkeller171@users.noreply.github.com>
2024-02-05 10:30:04 -0500

    Create MyProgram.java

---
+++ Unit 6 - Arrays/6.3 - Enhanced For Loops/6.3.3/MyProgram.java
@@ -0,0 +1,17 @@
+public class MyProgram
+{
+    public static void main(String[] args)
+    {
+        int[ ] values = {525, 111, 416, 914, 124, 811};
+        
+        // Use an enhanced for loop to iterate through the array
+        // Print out all values that are odd on their own line
+        for (int value: values){
+            if (value%2!=0){
+                System.out.print(value + " ");
+            }
+        }
+        
+        
+    }
+}
