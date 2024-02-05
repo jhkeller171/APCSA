a644082017a41f4e7cd89b1e4a260c9fcc9d0f29
James Keller <119902101+jhkeller171@users.noreply.github.com>
2024-02-05 10:28:55 -0500

    Create MyProgram.java

---
+++ Unit 6 - Arrays/6.3 - Enhanced For Loops/6.3.2/MyProgram.java
@@ -0,0 +1,55 @@
+public class MyProgram
+{
+    public static void main(String[] args)
+    {
+        int[] scores = {82, 95, 98, 78, 84};
+        
+        System.out.println("** Original Values **");
+        //Print the values of score using an Enhanced For Loop
+        for (int value: scores){
+            System.out.print(value + " ");
+        }
+        
+        
+        
+        //Print blank line in between
+        System.out.println();
+        
+        // Attempt to add 8 in Enhanced For Loop
+        for (int value: scores){
+            value += 8;
+        }
+       
+       
+        
+        System.out.println("** Adding in a Enhanced For Loop **");
+        //Re-print the values of score using an Enhanced For Loop
+        for (int value: scores){
+            System.out.print(value + " ");
+        }
+
+
+
+
+        //Print blank line in between
+        System.out.println();
+        
+        // Attempt to add 8 in Standard For Loop
+        for (int i=0;i<scores.length;i++){
+            scores[i] += 8;
+        }
+  
+  
+  
+      
+        System.out.println("** Adding in a Standard For Loop **");
+        //Re-print the values of score
+       for (int value: scores){
+            System.out.print(value + " ");
+        }
+       
+       
+       
+        System.out.println();
+    }
+}
