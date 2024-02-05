37752b3e29c897fe257db4f21daae0fb0a5b2b75
James Keller <119902101+jhkeller171@users.noreply.github.com>
2024-02-05 10:24:10 -0500

    Create MyProgram.java

---
+++ Unit 6 - Arrays/6.3 - Enhanced For Loops/MyProgram.java
@@ -0,0 +1,21 @@
+/*
+
+Given an array, use an Enhanced for loop to access and print each value.
+
+*/
+
+public class MyProgram
+{
+    public static void main(String[] args)
+    {
+        int[] tests = {90, 72, 81, 85, 98};
+        
+        // use an Enhanced for loop to print out 
+        // each value in this array on its own line
+        
+       for (int value: tests){
+           System.out.println(value);
+       } 
+       
+    }
+}
